6a2e06c881fd286ac84e2bf102cf42288b0e37be
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-03-13 18:46:43 -0400

    refactor member variables

--- blocker.py
+++ blocker.py
@@ -3,7 +3,6 @@ import experiments.tcp_connect
 EXPERIMENTS = []
 
 if __name__ == "__main__":
-    exp = experiments.tcp_connect.TCPConnectExperiment()
-
     with open("data/tcp_connect.txt") as fh:
-        print exp.run(fh)
+        exp = experiments.tcp_connect.TCPConnectExperiment(fh)        
+        print exp.run()
--- experiments/tcp_connect.py
+++ experiments/tcp_connect.py
@@ -1,23 +1,28 @@
 import socket
 
-def tcp_connect(host, port):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((host, port))
-        s.close()
-    except socket.error as msg:
-        return msg
-
 class TCPConnectExperiment():
-    def process_input(self, input):
-        for line in input:
-            yield line.split(' ')
-        
-    def run(self, input):
+    def __init__(self, input=None):
         if not input:
             raise Exception
 
-        for (host, port) in self.process_input(input):
-            err = tcp_connect(host, int(port))
+        self.input = input
+        self.host = None
+        self.port = None
+
+    def process_input(self):
+        for line in self.input:
+            yield line.split(' ')
+
+    def run(self):
+        for (self.host, self.port) in self.process_input():
+            err = self.tcp_connect()
 
             if err: return err
+
+    def tcp_connect(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.host, int(self.port)))
+            s.close()
+        except socket.error as msg:
+            return msg
