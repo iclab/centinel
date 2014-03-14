1d03f2a920be7a10f50a93d6c3fab7ecb9e3de81
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-03-14 16:56:36 -0400

    improve android app ui

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
+kivy.require('1.0.6')
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.listview import ListView
+from kivy.uix.boxlayout import BoxLayout
 
 import blocker
-import experiments.tcp_connect
-import experiments.http_request
 
-class MyApp(App):
+class BlockerApp(App):
     def build(self):
+        btn1 = Button(text="Start Tests")
+        btn1.bind(on_press=self.start_tests)
+
+        btn2 = Button(text="Show Results")
+        btn2.bind(on_press=self.btn_pressed)
+
+        buttons = BoxLayout(orientation='horizontal')
+        buttons.add_widget(btn1)
+        buttons.add_widget(btn2)
+
+        layout = BoxLayout(orientation="vertical")
+        layout.add_widget(buttons)
+
+        return layout
+
+    def start_tests(self, instance):
         blocker.run()
-        return Label(text='Hello world')
+
+    def btn_pressed(self, instance):
+        print instance.text
+
 
 if __name__ == '__main__':
-    MyApp().run()
+    BlockerApp().run()
