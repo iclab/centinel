a9d73d15943fa43f17c801522aed6c303d71ee86
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-03-14 11:48:48 -0400

    fixup

--- blocker.py
+++ blocker.py
@@ -1,11 +1,25 @@
+import os
+
 from datetime import datetime
+
 import experiments.tcp_connect
 import experiments.http_request
 
+RESULTS_DIR = "results"
+
 def run():
-    with open("results/"+datetime.now().isoformat()+"-tcp_connect.txt", "w") as out_fh:
+    experiments = [
+        experiments.tcp_connect,
+        experiments.http_request
+    ]
+
+    for exp in experiments:
+        result_file = "%s%s.txt" % (datetime.now().isoformat(), exp.name)
+        result_file = os.path.join(RESULTS_DIR, result_file)
+        result_file = open(result_file, "w")
+
         with open("data/tcp_connect.txt") as in_fh:
-            exp = experiments.tcp_connect.TCPConnectExperiment(in_fh, out_fh)
+            exp = experiments.tcp_connect.TCPConnectExperiment(in_fh, result_file)
             print exp.run()
 
     with open("data/http_request.txt") as fh:
