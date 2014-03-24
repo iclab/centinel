02f8911d0618aa4ee6e6aa07ed1716158206bbe6
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-03-24 19:09:18 -0400

    refactor tcp connect

--- experiments/tcp_connect.py
+++ experiments/tcp_connect.py
@@ -1,15 +1,13 @@
 import socket
+import json
 
 class TCPConnectExperiment:
     name = "tcp_connect"
 
-    def __init__(self, input_file=None, result_file=None):
-        if not input_file:
-            raise Exception
-
+    def __init__(self, input_file, result_file):
         self.input_file = input_file
         self.result_file = result_file
-        self.result = None
+        self.results = []
         self.host = None
         self.port = None
 
@@ -20,15 +18,21 @@ class TCPConnectExperiment:
     def run(self):
         for (self.host, self.port) in self.process_input():
             self.tcp_connect()
-            self.result_file.write(self.result)
+
+        json.dump(self.results, self.result_file)
 
     def tcp_connect(self):
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((self.host, int(self.port)))
-            s.close()
-            msg = True
-        except Exception as err:
-            msg = err.strerror
+        result = {
+            "host" : self.host,
+            "port" : self.port
+        }
 
-        self.result = "%s:%s %s" % (self.host, self.port, msg)
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((self.host, int(self.port)))
+            sock.close()
+            result["success"] = "true"
+        except Exception as err:
+            result["failure"] = str(err)
+
+        self.results.append(result)
