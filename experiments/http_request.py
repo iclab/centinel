29313bc101e320a49cb590287f7d9741f2140c16
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-03-24 19:03:35 -0400

    do some yak shaving

--- experiments/http_request.py
+++ experiments/http_request.py
@@ -4,9 +4,10 @@ import httplib
 class HTTPRequestExperiment:
     name = "http_request"
 
-    def __init__(self, input_file=None, result_file=None):
-        self.input_file = input_file
+    def __init__(self, input_file, result_file):
+        self.input_file  = input_file
         self.result_file = result_file
+        self.results = []
         self.host = None
         self.path = "/"
 
@@ -17,33 +18,38 @@ class HTTPRequestExperiment:
     def run(self):
         for self.host in self.process_input():
             self.http_request()
-            json.dump(self.result, self.result_file)
+
+        json.dump(self.results, self.result_file)
 
     def http_request(self):
-        self.result = {}
+        request  = {
+            "host" : self.host,
+            "path" : self.path
+        }
+
+        response = {}
         
         try:
             conn = httplib.HTTPConnection(self.host)
             conn.request("GET", self.path)
 
-            response = conn.getresponse()
-            body     = response.read()
-            headers  = response.getheaders()
+            resp = conn.getresponse()
+            response["status"] = resp.status
+            response["reason"] = resp.reason
+
+            headers = resp.getheaders()
+            response["headers"] = headers
+
+            body = resp.read()
+            response["body"] = body
 
             conn.close()
-
-            self.result = {
-                "http_version" : response.version,
-                "body"         : body,
-                "headers"      : dict(headers),
-                "status"       : response.status,
-                "reason"       : response.reason,
-            }
-
         except Exception as err:
-            self.result = {
-                "failure" : str(err)
-            }
+            response["failure"] = str(err)
 
-        self.result["host"] = self.host
-        self.result["path"] = self.path
+        result = {
+            "response" : response,
+            "request"  : request
+        }
+        
+        self.results.append(result)
