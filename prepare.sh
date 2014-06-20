fc18e702bd81464989e8307e40acd574d42f1c86
Abbas Razaghpanah <a.razzaghpanah@gmail.com>
2014-06-20 18:13:57 -0400

    Separated python dependency list for Mac from Linux.

--- prepare.sh
+++ prepare.sh
@@ -2,14 +2,14 @@
 
 
 if [ `uname -s` = "Linux" ]; then
-    for dep in `cat .pydeps`
+    for dep in `cat .pydeps-linux`
     do
 	sudo apt-get install python-$dep
     done;
 fi;
 
 if [ `uname -s` = "Darwin" ]; then
-    for dep in `cat .pydeps`
+    for dep in `cat .pydeps-mac`
     do
         pip install $dep
     done;
