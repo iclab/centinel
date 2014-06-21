53cdcc487349e98d95bb46f46fb93e472b390065
Abbas Razaghpanah <a.razzaghpanah@gmail.com>
2014-06-20 22:24:00 -0400

    Added the helper scripts (run and prepare).

---
+++ prepare.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+
+if [ `uname -s` = "Linux" ]; then
+    for dep in `cat .pydeps-linux`
+    do
+	sudo apt-get install python-$dep
+    done;
+fi;
+
+if [ `uname -s` = "Darwin" ]; then
+    for dep in `cat .pydeps-mac`
+    do
+        pip install $dep
+    done;
+    sudo port install scapy
+fi;
+
+sudo python setup.py build
