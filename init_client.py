1bacff1b490d92f06706e165fd597965d7a0ae3a
Abbas Razaghpanah <a.razzaghpanah@gmail.com>
2014-06-24 23:57:17 -0400

    Added client key directory creation to client initializer.

--- init_client.py
+++ init_client.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 import sys
+import os
+from os import path
+from os.path import exists
+
 
 from centinel.serverconnection import ServerConnection
 from centinel.utils.colors import bcolors
@@ -9,6 +13,13 @@ from centinel.client_config import client_conf
 selection = sys.argv
 selection.pop(0)
 c = client_conf()
+
+def check_create_dir(path):
+    if not os.path.exists(path):
+        print "Creating directory in %s" % (path)
+        os.makedirs(path)
+
+check_create_dir(c.c['client_keys_dir'])
 
 print bcolors.OKBLUE + 'Initializing the client.' + bcolors.ENDC
 if c.config_read:
