43f19c24b15f95c84ea1fa8f018444b80ef567a7
Abbas Razaghpanah <a.razzaghpanah@gmail.com>
2014-06-24 23:00:03 -0400

    Init client updated.

--- init_client.py
+++ init_client.py
@@ -10,20 +10,20 @@ selection = sys.argv
 selection.pop(0)
 c = conf()
 
-print bcolors.OKBLUE + 'Starting the exepriments.' + bcolors.ENDC
+print bcolors.OKBLUE + 'Initializing the client.' + bcolors.ENDC
 if c.config_read:
-    print bcolors.WARNING + 'A configuration file already exists for %s, are you sure you want to initialize? (if so, type \'yes\') ' + bcolors.ENDC
+    print bcolors.FAIL + 'A configuration file already exists for %s, are you sure you want to initialize? (if so, type \'yes\') ' %(c.c['client_tag']) + bcolors.ENDC
     ans = raw_input()
     if ans.lower() <> "yes":
 	print bcolors.OKBLUE + 'Nothing changed, exiting.' + bcolors.ENDC
 	exit(0)
 
 
-print bcolors.OKBLUE + 'Starting sync.' + bcolors.ENDC
+print bcolors.OKBLUE + 'Connecting to server...' + bcolors.ENDC
 serverconn = ServerConnection()
 if not serverconn.connect():
     exit(1)
 
-#serverconn.sync_results()
+serverconn.initialize_client()
 
 serverconn.disconnect()
