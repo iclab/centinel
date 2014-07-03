d9202d46f966cd6e02c6415e88a07136b8a8b452
Tikitaco <nytikitaco@gmail.com>
2014-07-03 16:55:00 -0400

    Added country dropdown list

--- web_server/activation_form.php
+++ web_server/activation_form.php
@@ -6,7 +6,7 @@ echo "ICLab Test Unit Activation Form";
 echo "</title>\n";
 echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/general_style.css\" />\n";
 echo "</head>\n";
-echo "<body>";
+echo "<body onload=\"javascript:load_countries()\">";
 echo "<div id=\"mom\">\n
 <div id=\"headtitle\">
 ICLab<br/>
@@ -23,11 +23,13 @@ if (window.XMLHttpRequest)
 {   // code for IE7+, Firefox, Chrome, Opera, Safari
     xmlhttp=new XMLHttpRequest();
     xmlhttpconsent=new XMLHttpRequest();
+    xmlhttpcountries=new XMLHttpRequest();
 }
 else
 {	// code for IE6, IE5
     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
     xmlhttpconsent=new ActiveXObject("Microsoft.XMLHTTP");
+    xmlhttpcountries=new ActiveXObject("Microsoft.XMLHTTP");
 }
 function validemail(email) {
  
@@ -41,7 +43,8 @@ function validemail(email) {
 
 function show_consent_form()
 {
-    country = document.getElementById("country").value;
+    var dropdown = document.getElementById("countrydropdown");
+    country = dropdown.options[dropdown.selectedIndex].text;
     xmlhttpconsent.onreadystatechange=function()
     {
         if (xmlhttpconsent.readyState==4 && xmlhttpconsent.status==200)
@@ -61,6 +64,29 @@ function show_consent_form()
     xmlhttpconsent.open("POST",getstr,true);
     xmlhttpconsent.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
     xmlhttpconsent.send(args);
+}
+
+function load_countries() 
+{
+    alert("Loaded");
+    var dropdown = document.getElementById("countrydropdown");
+    xmlhttpcountries.onreadystatechange=function()
+    {
+        if (xmlhttpcountries.readyState==4 && xmlhttpcountries.status==200)
+    	{
+    	    if(xmlhttpcountries.responseText == "Error")
+    	    {
+		alert("Country error!");
+		return;
+    	    }
+	    alert("Set Inner HTML");
+            dropdown.innerHTML = xmlhttpcountries.responseText;
+	}
+    }
+    xmlhttpcountries.open("POST","country_list.php",true);
+    xmlhttpcountries.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttpcountries.send();
+   
 }
 
 function sendMsg()
@@ -125,6 +151,11 @@ Please enter the information required to activate the test device:<br/>
 <tr><td>Client Tag:</td><td><input id="client_tag" type="text" value="" /></td></tr>
 <tr><td>Email:</td><td><input id="email" type="text" value="" /></td></tr>
 <tr><td>Country:</td><td><input id="country" type="text" value="" onchange="javascript:show_consent_form()" /></td></tr>
+<tr><td>Country Dropdown:</td><td><select id="countrydropdown" onchange="javascript:show_consent_form()">
+  <option value="Other">Other</option>
+  <option value="Russia">Russia</option>
+  <option value="China">China</option>
+</select></td></tr>
 <tr><td> </td><td><input type="button" onclick="javascript:sendMsg()" value="Send" /></td></tr>
 </table>
 </td>
