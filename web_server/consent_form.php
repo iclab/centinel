44041f1563b9713fbabce203e3d686e909c7a92d
Tikitaco <nytikitaco@gmail.com>
2014-07-03 15:18:05 -0400

    Added file reading from forms and country list

--- web_server/consent_form.php
+++ web_server/consent_form.php
@@ -6,6 +6,13 @@ if(!isset($_POST["country"]))
     exit;
 }
 
-echo $_POST["country"];
+$path = './forms/' . $_POST["country"] . '.txt';
+if(file_exists($path)) {
+$file = file_get_contents($path, FILE_USE_INCLUDE_PATH);
+}
+else {
+$file = file_get_contents("./forms/Generic.txt", FILE_USE_INCLUDE_PATH);
+}
+echo($file)
 
 ?>
