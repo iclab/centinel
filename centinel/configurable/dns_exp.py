dcb2bc877241d433ff030191fb0323ca24ea206b
Tikitaco <nytikitaco@gmail.com>
2014-07-10 17:10:26 -0400

    Added timeout field for config and better error handling for timeouts

--- centinel/configurable/dns_exp.py
+++ centinel/configurable/dns_exp.py
@@ -31,6 +31,11 @@ class ConfigurableDNSExperiment(Experiment):
         else:
             self.record = 'A'
 
+        if 'timeout' in self.args.keys():
+            self.timeout = int(self.args['timeout'])
+        else:
+            self.timeout = 3
+
 	url_list = parser.items('URLS')
 	for url in url_list[0][1].split():
 	    self.host = url
@@ -52,17 +57,22 @@ class ConfigurableDNSExperiment(Experiment):
         else:
             try:
                 query = dns.message.make_query(self.host, self.record)
-                response = dns.query.udp(query, self.resolver, timeout=3)
+                response = dns.query.udp(query, self.resolver, timeout=self.timeout)
                 for answer in response.answer:
                     if ans == "":
                         ans = answer.to_text()
                     else:
                         ans += ", " + answer.to_text()
+            except dns.exception.Timeout:
+                print("Query Timed out for " + self.host)
+                ans = "Timeout"
             except Exception:
                 print("Error Querying " + self.record + " record for " + self.host)
+                ans = "Error"
         if ans == "":
             ans = "Unavailable"
 	print(ans)
+
         result["record_type"] = self.record
         result['record'] = ans
         
