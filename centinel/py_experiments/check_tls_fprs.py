841b91cfc03aea68a7921e3ba68ead6f34e5e123
Ben Jones <benjam2@g.clemson.edu>
2014-07-11 17:51:59 -0700

    added inital version of tls test
    added a test that will check for man-in-the-middle attacks on
    ssl/tls connections on arbitrary hosts and ports

---
+++ centinel/py_experiments/check_tls_fprs.py
@@ -0,0 +1,37 @@
+import os
+import M2Crypto
+import ssl
+
+from centinel.experiment_py import Experiment
+from centinel.utils import tls
+
+class TLSExperiment(Experiment):
+    """Check the tls fingerprints of a site"""
+    name = "tls"
+
+    def __init__(self, input_file):
+        self.input_file  = input_file
+        self.results = []
+
+    def run(self):
+        for line in self.input_file:
+            line = line.split(',')
+            self.host, self.port = line[0].strip(), int(line[1].strip())
+            self.fprs = []
+            for entry in line[2:]:
+                self.fprs.append(entry.strip().lower())
+            self.tls_test()
+
+    def tls_test(self):
+        result = {"host": self.host }
+
+        print "Getting TLS Certificate from {0} on port {1} ".format(host,
+                                                                     port)
+        fpr = tls.get_fingerprint(host, port)
+        result['fpr'] = fpr
+        if fpr in self.fprs:
+            result["success"] = 'true'
+        else:
+            result["success"] = 'false'
+
+        self.results.append(result)
