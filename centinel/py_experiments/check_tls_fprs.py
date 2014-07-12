1897912254bf022c0fd73ae97cbce9e85739a756
Ben Jones <benjam2@g.clemson.edu>
2014-07-11 17:55:37 -0700

    updated for pep compliance

--- centinel/py_experiments/check_tls_fprs.py
+++ centinel/py_experiments/check_tls_fprs.py
@@ -1,16 +1,13 @@
-import os
-import M2Crypto
-import ssl
-
 from centinel.experiment_py import Experiment
 from centinel.utils import tls
+
 
 class TLSExperiment(Experiment):
     """Check the tls fingerprints of a site"""
     name = "tls"
 
     def __init__(self, input_file):
-        self.input_file  = input_file
+        self.input_file = input_file
         self.results = []
 
     def run(self):
@@ -23,11 +20,11 @@ class TLSExperiment(Experiment):
             self.tls_test()
 
     def tls_test(self):
-        result = {"host": self.host }
+        result = {"host": self.host}
 
-        print "Getting TLS Certificate from {0} on port {1} ".format(host,
-                                                                     port)
-        fpr = tls.get_fingerprint(host, port)
+        print "Getting TLS Certificate from {0} on port {1} ".format(self.host,
+                                                                     self.port)
+        fpr = tls.get_fingerprint(self.host, self.port)
         result['fpr'] = fpr
         if fpr in self.fprs:
             result["success"] = 'true'
