efb04d1cb022a9e971950fbb96afc6bcb0e7fd30
Tikitaco <nytikitaco@gmail.com>
2014-07-15 15:47:05 -0400

    Removed redunant data recording and replaced ||| with ;

--- centinel/py_experiments/indonesia.py
+++ centinel/py_experiments/indonesia.py
@@ -98,9 +98,7 @@ class IndExperiment(Experiment):
                 	ip = stripped
             	    if not '=' in stripped and '.' in stripped and not self.isIp(stripped):
                 	reverseDns = stripped
-		results["Hop" + str(t) + "Ip"] = ip
-		results["Hop" + str(t) + "ReverseDns"] = reverseDns
-        	complete_traceroute += ip + "|||" + reverseDns
+        	complete_traceroute += ip + ";" + reverseDns
         	if ip == "Not Found" and reverseDns != "Not Found":
             	    pass
         	if ip == finalIp or t == max_hops:
