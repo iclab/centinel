c510b18e8efba150733c12445150b3d5e24ccd79
Abbas Razaghpanah <a.razzaghpanah@gmail.com>
2014-07-15 17:50:56 -0400

    edited the schema so that it could be loaded into MySQL

--- database/schema.sql
+++ database/schema.sql
@@ -2,13 +2,34 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `sirocco` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `sirocco` ;
+
 
 -- -----------------------------------------------------
--- Table `mydb`.`countries`
+-- Table `sirocco`.`clients`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`countries` (
+CREATE TABLE IF NOT EXISTS `sirocco`.`clients` (
+  `tag` VARCHAR(20) NOT NULL,
+  `owner_email` VARCHAR(100) NULL,
+  `authorized` INT NULL,
+  `public_key` LONGTEXT NULL,
+  PRIMARY KEY (`tag`),
+  UNIQUE INDEX `tag_UNIQUE` (`tag` ASC),
+  INDEX `user_idx` (`owner_email` ASC),
+  CONSTRAINT `user`
+    FOREIGN KEY (`owner_email`)
+    REFERENCES `sirocco`.`users` (`email`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+COMMENT = 'Client instance information including tag, owner, and autori /* comment truncated */ /*zation status.*/';
+
+
+-- -----------------------------------------------------
+-- Table `sirocco`.`countries`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sirocco`.`countries` (
   `id` INT(11) NOT NULL,
   `name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`),
@@ -18,10 +39,10 @@ COMMENT = 'Static country list.';
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`users`
+-- Table `sirocco`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`users` (
-  `email` TEXT NOT NULL,
+CREATE TABLE IF NOT EXISTS `sirocco`.`users` (
+  `email` VARCHAR(100) NOT NULL,
   `name` TEXT NULL,
   `country` INT(11) NULL,
   `passwd` VARCHAR(45) NULL,
@@ -30,7 +51,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`users` (
   INDEX `country_idx` (`country` ASC),
   CONSTRAINT `country`
     FOREIGN KEY (`country`)
-    REFERENCES `mydb`.`countries` (`id`)
+    REFERENCES `sirocco`.`countries` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -38,37 +59,16 @@ COMMENT = 'Users who have completed the registration form, will have a  /* comme
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`clients`
+-- Table `sirocco`.`client_locations`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`clients` (
-  `tag` TEXT NOT NULL,
-  `owner_email` TEXT NULL,
-  `authorized` INT NULL,
-  `public_key` LONGTEXT NULL,
-  PRIMARY KEY (`tag`),
-  UNIQUE INDEX `tag_UNIQUE` (`tag` ASC),
-  INDEX `user_idx` (`owner_email` ASC),
-  CONSTRAINT `user`
-    FOREIGN KEY (`owner_email`)
-    REFERENCES `mydb`.`users` (`email`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf32
-COMMENT = 'Client instance information including tag, owner, and autori /* comment truncated */ /*zation status.*/';
-
-
--- -----------------------------------------------------
--- Table `mydb`.`client_locations`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`client_locations` (
-  `tag` TEXT NULL,
-  `location` TEXT NULL,
+CREATE TABLE IF NOT EXISTS `sirocco`.`client_locations` (
+  `tag` VARCHAR(20) NULL,
+  `location` VARCHAR(200) NULL,
   `timestamp` DATETIME NULL,
   INDEX `tag_idx` (`tag` ASC),
   CONSTRAINT `tag`
     FOREIGN KEY (`tag`)
-    REFERENCES `mydb`.`clients` (`tag`)
+    REFERENCES `sirocco`.`clients` (`tag`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
