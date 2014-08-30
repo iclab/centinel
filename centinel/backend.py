4f5b171b972b2255dfc3cdb8eea8b4a2745ae437
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-08-30 17:02:12 -0400

    Send results to the server

--- centinel/backend.py
+++ centinel/backend.py
@@ -1,13 +1,14 @@
+import os
+import glob
 import requests
+
 import config
 
 def request(slug):
     url = "%s%s" % (config.server_url, slug)
     req = requests.get(url)
 
-    if req.status_code != requests.codes.ok:
-        raise req.raise_for_status()
-
+    req.raise_for_status()
     return req.json()
 
 def get_recommended_versions():
@@ -26,7 +27,14 @@ def submit_result(file_name):
     with open(file_name) as result_file:
         file = {'result' : result_file}
         url = "%s%s" % (config.server_url, "/results")
-        requests.post(url, files=file)
+        req = requests.post(url, files=file)
 
-    if req.status_code != requests.codes.ok:
-        raise req.raise_for_status()
+    req.raise_for_status()
+
+def sync():
+    # send all results
+    for path in glob.glob(os.path.join(config.results_dir,'[!_]*.json')):
+        try:
+            submit_result(path)
+        except Exception, e:
+            logging.error("Unable to send result file %s" % (path))
