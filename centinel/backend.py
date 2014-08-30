182857c9180f7b4e2db06ffc43c37582c0ce46e7
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-08-30 17:40:25 -0400

    Add ability to download file

--- centinel/backend.py
+++ centinel/backend.py
@@ -5,23 +5,23 @@ import requests
 import config
 
 def request(slug):
-    url = "%s%s" % (config.server_url, slug)
+    url = "%s/%s" % (config.server_url, slug)
     req = requests.get(url)
 
     req.raise_for_status()
     return req.json()
 
 def get_recommended_versions():
-    return request("/versions")
+    return request("versions")["versions"]
 
 def get_experiments():
-    return request("/experiments")
+    return request("experiments")["experiments"]
 
 def get_results():
-    return request("/results")
+    return request("results")
 
 def get_clients():
-    return request("/clients")
+    return request("clients")
 
 def submit_result(file_name):
     with open(file_name) as result_file:
@@ -31,6 +31,10 @@ def submit_result(file_name):
 
     req.raise_for_status()
 
+def download_experiment(name):
+    url = "%s/%s/%s" % (config.server_url, "experiments", name)
+    req = requests.get(url)
+
 def sync():
     # send all results
     for path in glob.glob(os.path.join(config.results_dir,'[!_]*.json')):
@@ -38,3 +42,15 @@ def sync():
             submit_result(path)
         except Exception, e:
             logging.error("Unable to send result file %s" % (path))
+
+    # get all experiment names
+    available_experiments = []
+    for path in glob.glob(os.path.join(config.experiments_dir,'[!_]*.py')):
+        file_name, _ = os.path.splitext(os.path.basename(path))
+        available_experiments.append(file_name)
+    available_experiments = set(available_experiments)
+
+    # download new experiments from server
+    for experiment in get_experiments():
+        if experiment not in available_experiments:
+            download_experiment(file_name)
