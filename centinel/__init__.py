e0f2430c209d9a9c7a0d3451c4d8133a274dc071
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-08-31 14:25:28 -0400

    Restructure

--- centinel/__init__.py
+++ centinel/__init__.py
@@ -2,3 +2,15 @@ __title__ = 'centinel'
 __version__ = '0.1.3'
 
 from . import client, backend
+
+class ExperimentList(type):
+    experiments = {}
+    def __init__(cls, name, bases, attrs):
+        if name != "Experiment":
+            ExperimentList.experiments[cls.name] = cls
+
+class Experiment:
+    __metaclass__ = ExperimentList
+
+    def run(self):
+        raise NotImplementedError
