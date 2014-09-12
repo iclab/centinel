38dfba50de40c5245a2b9887ba1eb798eefb8bbb
Ben Jones <benjam2@g.clemson.edu>
2014-09-12 09:20:25 -0400

    updated code for sathya's code review

--- vpn.py
+++ vpn.py
@@ -8,13 +8,13 @@
 
 import argparse
 import os
-import os.path
 
 import centinel.backend
 import centinel.client
 import centinel.config
 import centinel.openvpn
 import centinel.hma
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -27,17 +27,17 @@ def parse_args():
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--directory", "-d", dest='directory',
                        help="Directory with experiments, config files, etc.")
-    createConfHelp = ("Create configuration files for the given "
-                      "openvpn config files so that we can treat each "
-                      "one as a client. The argument should be a "
-                      "directory with a subdirectory called openvpn "
-                      "that contains the openvpn config files")
-    group.add_argument('--create-config', '-c', help=createConfHelp,
+    create_conf_help = ("Create configuration files for the given "
+                        "openvpn config files so that we can treat each "
+                        "one as a client. The argument should be a "
+                        "directory with a subdirectory called openvpn "
+                        "that contains the openvpn config files")
+    group.add_argument('--create-config', '-c', help=create_conf_help,
                        dest='createConfDir')
     return parser.parse_args()
 
 
-def scan_vpns(directory, authFile):
+def scan_vpns(directory, auth_file):
     """For each VPN, check if there are experiments and scan with it if
     necessary
 
@@ -50,36 +50,37 @@ def scan_vpns(directory, authFile):
     """
 
     # iterate over each VPN
-    vpnDir  = return_abs_path(directory, "vpns")
-    confDir = return_abs_path(directory, "configs")
-    authFile = return_abs_path(".", authFile)
-    vpn = centinel.openvpn.OpenVPN()
-    for filename in os.listdir(confDir):
-        vpnConfig = os.path.join(vpnDir, filename)
-        centConfig = os.path.join(confDir, filename)
-        vpn.start(vpnConfig, timeout=30, authFile=authFile)
+    vpn_dir = return_abs_path(directory, "vpns")
+    conf_dir = return_abs_path(directory, "configs")
+    auth_file = return_abs_path(".", auth_file)
+    for filename in os.listdir(conf_dir):
+        vpn_config = os.path.join(vpn_dir, filename)
+        cent_config = os.path.join(conf_dir, filename)
+        vpn = centinel.openvpn.OpenVPN(timeout=30, auth_file=auth_file,
+                                       config_file=vpn_config)
+        vpn.start()
         if not vpn.started:
             vpn.stop()
             continue
         # now that the VPN is started, get centinel to process the VPN
         # stuff and sync the results
         config = centinel.config.Configuration()
-        config.parse_config(centConfig)
+        config.parse_config(cent_config)
         client = centinel.client.Client(config.params)
         client.setup_logging()
         client.run()
         centinel.backend.sync(config.params)
         vpn.stop()
 
-
 def return_abs_path(directory, path):
     """Unfortunately, Python is not smart enough to return an absolute
     path with tilde expansion, so I writing functionality to do this
 
     """
+    if directory is None or path is None:
+        return
     directory = os.path.expanduser(directory)
     return os.path.abspath(os.path.join(directory, path))
-
 
 def create_config_files(directory):
 
@@ -94,34 +95,34 @@ def create_config_files(directory):
     -----results (contains the results)
 
     """
-    vpnDir  = return_abs_path(directory, "vpns")
-    confDir = return_abs_path(directory, "configs")
-    os.mkdir(confDir)
-    homeDirs = return_abs_path(directory, "home")
-    os.mkdir(homeDirs)
-    for filename in os.listdir(vpnDir):
+    vpn_dir = return_abs_path(directory, "vpns")
+    conf_dir = return_abs_path(directory, "configs")
+    os.mkdir(conf_dir)
+    home_dirs = return_abs_path(directory, "home")
+    os.mkdir(home_dirs)
+    for filename in os.listdir(vpn_dir):
         configuration = centinel.config.Configuration()
         # setup the directories
-        homeDir = os.path.join(homeDirs, filename)
-        os.mkdir(homeDir)
-        configuration.params['user']['centinel_home'] = homeDir
-        expDir = os.path.join(homeDir, "experiments")
-        os.mkdir(expDir)
-        configuration.params['dirs']['experiments_dir'] = expDir
-        dataDir = os.path.join(homeDir, "data")
-        os.mkdir(dataDir)
-        configuration.params['dirs']['data_dir'] = dataDir
-        resDir = os.path.join(homeDir, "results")
-        os.mkdir(resDir)
-        configuration.params['dirs']['results_dir'] = resDir
+        home_dir = os.path.join(home_dirs, filename)
+        os.mkdir(home_dir)
+        configuration.params['user']['centinel_home'] = home_dir
+        exp_dir = os.path.join(home_dir, "experiments")
+        os.mkdir(exp_dir)
+        configuration.params['dirs']['experiments_dir'] = exp_dir
+        data_dir = os.path.join(home_dir, "data")
+        os.mkdir(data_dir)
+        configuration.params['dirs']['data_dir'] = data_dir
+        res_dir = os.path.join(home_dir, "results")
+        os.mkdir(res_dir)
+        configuration.params['dirs']['results_dir'] = res_dir
 
-        logFile = os.path.join(homeDir, "centinel.log")
-        configuration.params['log']['log_file'] = logFile
-        loginFile = os.path.join(homeDir, "login")
-        configuration.params['server']['login_file'] = loginFile
+        log_file = os.path.join(home_dir, "centinel.log")
+        configuration.params['log']['log_file'] = log_file
+        login_file = os.path.join(home_dir, "login")
+        configuration.params['server']['login_file'] = login_file
 
-        confFile = os.path.join(confDir, filename)
-        configuration.write_out_config(confFile)
+        conf_file = os.path.join(conf_dir, filename)
+        configuration.write_out_config(conf_file)
 
 
 if __name__ == "__main__":
