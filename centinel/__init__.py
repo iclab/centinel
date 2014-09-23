f59248520648051f2a9d746983ea8e6dccbbb6c0
Ben Jones <benjam2@g.clemson.edu>
2014-09-23 10:47:03 -0400

    updated __init__ files to get everything

--- centinel/__init__.py
+++ centinel/__init__.py
@@ -3,6 +3,6 @@ __title__ = 'centinel'
 __version__ = '0.1.3'
 
 import centinel.backend
-import centinel.config
-import centinel.client
 import centinel.cli
+import centinel.client
+import centinel.config
