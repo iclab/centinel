e7dcc7def43d6cb75fa11784c5e1c8dd18f3fbf4
Ben Jones <benjam2@g.clemson.edu>
2014-09-23 10:08:57 -0400

    changed our entry points
    changed our code to use centinel.py and vpn.py as entry points (so
    these programs can be accessed as normal binaries on the platform)

--- centinel/__init__.py
+++ centinel/__init__.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python
+__title__ = 'centinel'
+__version__ = '0.1.3'
 
 import centinel.backend
 import centinel.config
 import centinel.client
-
-
-def run():
-    """This is the entry point for the build script, aka where you can run
-    everything without specifying any arguments
-
-    """
-    configuration = centinel.config.Configuration()
-    client = centinel.client.Client(configuration.params)
-    client.setup_logging()
-    # to make everything self contained, we are syncing before and
-    # after each run
-    centinel.backend.sync(configuration.params)
-    client.run()
-    centinel.backend.sync(configuration.params)
+import centinel.cli
