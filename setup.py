0ffeb0976287c2b9aeb9aba76c3eaa39ff79cc31
Ben Jones <benjam2@g.clemson.edu>
2014-09-23 10:48:11 -0400

    updated setup.py
    updated setup.py with cli changes, new package name, and an
    additional dependency on requests (since we use it)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ DESCRIPTION = """\
 Centinel is a tool used to detect network interference and internet censorship."""
 
 setup(
-    name = "centinel",
+    name = "centinel-dev",
     version = "0.1.4.4",
     author = "Sathyanarayanan Gunasekaran",
     author_email = "gsathya@gatech.edu",
@@ -13,10 +13,12 @@ setup(
     license = "MIT",
     keywords = "censorship network interference",
     url = "https://www.github.com/iclab/centinel",
-    packages = ["centinel", "centinel.experiments", "centinel.primitives", "centinel.vpn"],
-    install_requires = ["dnspython >= 1.11.0"],
+    packages = ["centinel", "centinel.experiments", "centinel.primitives",
+                "centinel.vpn"],
+    install_requires = ["dnspython >= 1.11.0", "requests >= 2.0.0"],
     include_package_data = True,
     entry_points = {
-        'console_scripts': ['centinel-dev=centinel:run']
+        'console_scripts': ['centinel-dev=centinel.cli:run',
+                            'centinel-vpn-dev=centinel.vpn.cli:run']
     },
 )
