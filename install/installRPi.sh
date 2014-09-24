1cc00eaecc0157b887c3c7aa8d7d8c844384783b
Ben Jones <benjam2@g.clemson.edu>
2014-09-24 07:27:45 -0400

    added daily autoupdate checker

--- install/installRPi.sh
+++ install/installRPi.sh
@@ -11,3 +11,9 @@ sudo echo "# cron job for centinel" >> /etc/cron.hourly/centinel
 sudo echo "/usr/local/bin/centinel-dev" >> /etc/cron.hourly/centinel
 sudo echo "/usr/local/bin/centinel-dev --sync" >> /etc/cron.hourly/centinel
 sudo chmod +x /etc/cron.hourly/centinel
+
+# write out a centinel autoupdate script
+sudo echo "#!/bin/bash" > /etc/cron.daily/centinel
+sudo echo "# cron job for centinel autoupdate" >> /etc/cron.daily/centinel-autoupdate
+sudo echo "sudo pip install --upgrade centinel-dev" >> /etc/cron.daily/centinel-autoupdate
+sudo chmod +x /etc/cron.daily/centinel-autoupdate
