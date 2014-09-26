9bdcc90425a5850dda4f240037e0d3264f527cda
Ben Jones <benjam2@g.clemson.edu>
2014-09-26 18:44:06 -0400

    added params for default config and consent

--- centinel.py
+++ centinel.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
-import sys
 import argparse
+import getpass
+import os
+import sys
 
 import centinel
 import centinel.config
+
+# Constants
+DEFAULT_CONFIG_FILE = os.path.expanduser('~' + getpass.getuser())
 
 
 def parse_args():
@@ -17,22 +22,49 @@ def parse_args():
                         nargs="*", dest="experiments")
     parser.add_argument('--config', '-c', help='Configuration file',
                         dest='config')
+
+    give_consent_help = ('Give informed consent so you can upload results to '
+                         'the researchers at ICLab and download their '
+                         'experiments')
+    parser.add_argument('--give-informed-consent', help=give_consent_help,
+                        dest='given_consent', action='store_true',
+                        default=None)
+    clear_consent_help = ('Choose not to give consent to Centinel and clear '
+                          'the consent flag so that you can run Centinel. '
+                          'Note: This is *not* advised; you are undertaking '
+                          'a lot of risk and you will not be able to upload '
+                          'results to ICLab or download experiments. Even if '
+                          'you are choosing not to upload your results, '
+                          'please visit our site to better understand the '
+                          'risks you face.')
+    parser.add_argument('--do-not-give-informed-consent',
+                        help=clear_consent_help, dest='given_consent',
+                        action='store_true', default=None)
+
+                        
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
 
-    # create the configuration at runtime (what we were doing before)
-    # by default, but use the user specified file parameters instead
-    # if specified
+    # we need to store some persistent info, so check if a config file
+    # exists (default location is ~/.centinel/config.ini). If the file
+    # does not exist, then create a new one at run time
     configuration = centinel.config.Configuration()
     if args.config:
         configuration.parse_config(args.config)
+    else:
+        # if the file does not exist, then the default config file
+        # will be used
+        if os.path.exists(DEFAULT_CONFIG_FILE):
+            configuration.parse_config(DEFAULT_CONFIG_FILE)
 
     client = centinel.client.Client(configuration.params)
     client.setup_logging()
 
     if args.sync:
         centinel.backend.sync(configuration.params)
+    elif args.given_consent is not None:
+        client.informed_consent(args.given_consent)
     else:
         client.run()
