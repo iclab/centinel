43b8173f889c0f4008314f2a3ac051481508d6ec
Ben Jones <benjam2@g.clemson.edu>
2014-09-29 00:15:21 -0400

    bug fixed config stuff so it actually works

--- centinel.py
+++ centinel.py
@@ -2,31 +2,30 @@
 import argparse
 import getpass
 import os
-import sys
 
 import centinel
 import centinel.config
 
 # Constants
 DEFAULT_CONFIG_FILE = os.path.expanduser('~' + getpass.getuser() +
-                                         "config.ini")
+                                         "/.centinel/config.ini")
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', '-c', help='Configuration file',
                         dest='config')
-    group =  parser.add_mutually_exclusive_group()
+    group = parser.add_mutually_exclusive_group()
     group.add_argument('--version', '-v', action='version',
-                        version="Centinel %s" % (centinel.__version__),
-                        help='Sync data with server')
+                       version="Centinel %s" % (centinel.__version__),
+                       help='Sync data with server')
     group.add_argument('--sync', help='Sync data with server',
-                        action='store_true')
+                       action='store_true')
     consent_help = ("Give informed consent so that you can download "
                     "experiments from the researchers at ICLab and upload "
                     "results for analysis")
     group.add_argument('--informed-consent', help=consent_help,
-                        dest='consent', default=False, action='store_true')
+                       dest='consent', default=False, action='store_true')
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -43,6 +42,7 @@ if __name__ == "__main__":
         # will be used
         if os.path.exists(DEFAULT_CONFIG_FILE):
             configuration.parse_config(DEFAULT_CONFIG_FILE)
+        else:
             configuration.write_out_config(DEFAULT_CONFIG_FILE)
 
     client = centinel.client.Client(configuration.params)
