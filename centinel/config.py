8c6cac225a0d1a0dd2f82b2137f88cf4cafb0488
Abbas Razaghpanah <arazaghpanah@cs.stonybrook.edu>
2014-10-02 15:20:41 -0400

    added test subsetting for the client

--- centinel/config.py
+++ centinel/config.py
@@ -41,6 +41,12 @@ class Configuration():
         # "centinel.log")
         self.params['log']['log_format'] = '%(asctime)s: %(levelname)s: %(message)s'
 
+        # experiments
+        experiments = {}
+        experiments['random_subset'] = True
+        experiments['subset_size'] = 2
+        self.params['experiments'] = experiments
+
         # server
         servers = {}
         servers['server_url'] = "https://server.iclab.org:8082"
