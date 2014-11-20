b675f2fc1aa89120953b0a83adc1de6869481878
Abbas Razaghpanah <arazaghpanah@cs.stonybrook.edu>
2014-11-20 12:54:08 -0500

    bumped the version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Centinel is a tool used to detect network interference and internet censorship."
 
 setup(
     name = "centinel-dev",
-    version = "0.1.5.0",
+    version = "0.1.5.1",
     author = "Sathyanarayanan Gunasekaran",
     author_email = "gsathya@gatech.edu",
     description = DESCRIPTION,
