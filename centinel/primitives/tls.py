0e0006644f87ca314155d32b6a6c8e35d21d428e
Abbas Razaghpanah <arazaghpanah@cs.stonybrook.edu>
2015-02-23 14:40:25 -0500

    added SSLv3 fallback to TLSv1 in TLS primitive, fixes #123

--- centinel/primitives/tls.py
+++ centinel/primitives/tls.py
@@ -6,7 +6,13 @@ import M2Crypto
 
 
 def get_fingerprint(host, port):
-    cert = ssl.get_server_certificate((host, port))
+    try:
+        cert = ssl.get_server_certificate((host, port))
+    # if this fails, there's a possibility that SSLv3 handshake was
+    # attempted and rejected by the server. Use TLSv1 instead.
+    except ssl.SSLError as exp:
+        cert = ssl.get_server_certificate((host, port), ssl_version=ssl.PROTOCOL_TLSv1)
+
     x509 = M2Crypto.X509.load_cert_string(cert, M2Crypto.X509.FORMAT_PEM)
     fingerprint = x509.get_fingerprint('sha1')
     return fingerprint.lower(), cert
