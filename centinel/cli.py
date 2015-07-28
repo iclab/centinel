eec19b40b44f9a209e2f16ae1dfd4ad98a892f72
Ben Jones <benjam2@g.clemson.edu>
2015-07-28 12:45:02 -0400

    updated centinel to a format conducive to package entry
    updated centinel to be more like the dev branch by making the
    structure more conducive to package entry points

---
+++ centinel/cli.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+import argparse
+import getpass
+import os
+
+import centinel
+import centinel.config
+import centinel.daemonize
+
+# Constants
+DEFAULT_CONFIG_FILE = os.path.expanduser('~' + getpass.getuser() +
+                                         "/.centinel/config.ini")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', '-c', help='Configuration file',
+                        dest='config')
+    no_verify_help = ("Disable certificate verification (NOT RECOMMENDED! "
+                      "Use only when debugging.)")
+    parser.add_argument('--no-verify', '-nv', help=no_verify_help,
+                        action='store_true')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--version', '-v', action='version',
+                       version="Centinel %s" % (centinel.__version__),
+                       help='Print the installed version number')
+    group.add_argument('--sync', help='Sync data with server',
+                       action='store_true')
+    consent_help = ("Give informed consent so that you can download "
+                    "experiments from the researchers at ICLab and upload "
+                    "results for analysis")
+    group.add_argument('--informed-consent', help=consent_help,
+                       dest='consent', default=False, action='store_true')
+
+    daemon_help = ('Create cron jobs to run centinel in the background and '
+                   'autoupdate. You must be root to use this functionality'
+                   'By default, this will use /usr/local/bin/centinel-dev'
+                   'for the binary location and will create an autoupdate '
+                   'script')
+    parser.add_argument('--daemonize', help=daemon_help, action='store_true',
+                        dest='daemonize')
+    binary_help = ('Name or location of the binary to use in the cron job '
+                   'for centinel')
+    parser.add_argument('--binary', help=binary_help,
+                        default='/usr/local/bin/centinel-dev')
+    update_help = ('Create an autoupdate script for the installed package. '
+                   'Note that you must have installed from a pip package for '
+                   'this to work correctly and you must also set the '
+                   'daemonize option')
+    parser.add_argument('--auto-update', action='store_false',
+                        help=update_help, default="centinel-dev")
+
+    args = parser.parse_args()
+    if not args.daemonize and (args.auto_update != 'centinel-dev' or
+                               args.binary != '/usr/local/bin/centinel-dev'):
+        parser.error("--auto-update and --binary must be used with "
+                     "--daemonize")
+    return args
+
+
+def run():
+    """Entry point for package and cli uses"""
+
+    args = parse_args()
+
+    # we need to store some persistent info, so check if a config file
+    # exists (default location is ~/.centinel/config.ini). If the file
+    # does not exist, then create a new one at run time
+    configuration = centinel.config.Configuration()
+    if args.config:
+        configuration.parse_config(args.config)
+    else:
+        # if the file does not exist, then the default config file
+        # will be used
+        if os.path.exists(DEFAULT_CONFIG_FILE):
+            configuration.parse_config(DEFAULT_CONFIG_FILE)
+        else:
+            print 'Configuration file does not exist.'
+
+        if not ('version' in configuration.params and \
+            configuration.params['version']['version'] == centinel.__version__):
+            print ('Configuration file is from a different version of '
+                   'Centinel.')
+            configuration = centinel.config.Configuration()
+
+        configuration.write_out_config(DEFAULT_CONFIG_FILE)
+
+    client = centinel.client.Client(configuration.params)
+    client.setup_logging()
+
+    # disable cert verification if the flag is set
+    if args.no_verify:
+        configuration.params['server']['verify'] = False
+
+    user = centinel.backend.User(configuration.params)
+    # Note: because we have mutually exclusive arguments, we don't
+    # have to worry about multiple arguments being called
+    if args.sync:
+        centinel.backend.sync(configuration.params)
+    elif args.consent:
+        user.informed_consent()
+    elif args.daemonize:
+        # if we don't have a valid binary location, then exit
+        if not os.path.exists(args.binary):
+            print "Error: no binary found to daemonize"
+            exit(1)
+        centinel.daemonize.daemonize(args.auto_update, args.binary)
+    else:
+        client.run()
+
+if __name__ == "__main__":
+    run()
---
+++ centinel/vpn/cli.py
@@ -0,0 +1,234 @@
+#!/usr/bin/python
+# vpn.py: a top level interface to commands to run VPNs on a VM as
+# different clients. The use case is allowing us to measure from more
+# places.
+
+import argparse
+import logging
+from random import shuffle
+import os
+
+import centinel.backend
+import centinel.client
+import centinel.config
+import centinel.vpn.openvpn as openvpn
+import centinel.vpn.hma as hma
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--auth-file', '-u', dest='auth_file', default=None,
+                        help=("File with HMA username on first line, \n"
+                              "HMA password on second line"))
+    parser.add_argument('--create-hma-configs', dest='create_HMA',
+                        action="store_true",
+                        help='Create the openvpn config files for HMA')
+    parser.add_argument('--shuffle', '-s', dest='shuffle_lists',
+                        action="store_true",
+                        help='Randomize the order of vantage points')
+    parser.add_argument('--exclude', '-e', dest='exclude_list', default=None,
+                        help=('Countries to exclude when scanning (comma '
+                              'separated two letter country codes)'))
+    parser.add_argument('--log-file', '-l', dest='log_file', default=None,
+                        help="Log file location")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--directory', '-d', dest='directory',
+                       help='Directory with experiments, config files, etc.')
+    create_conf_help = ('Create configuration files for the given '
+                        'openvpn config files so that we can treat each '
+                        'one as a client. The argument should be a '
+                        'directory with a subdirectory called openvpn '
+                        'that contains the openvpn config files')
+    group.add_argument('--create-config', '-c', help=create_conf_help,
+                       dest='create_conf_dir')
+    return parser.parse_args()
+
+
+def scan_vpns(directory, auth_file, exclude_list, shuffle_lists=False):
+    """For each VPN, check if there are experiments and scan with it if
+    necessary
+
+    Note: the expected directory structure is
+    args.directory
+    -----vpns (contains the OpenVPN config files
+    -----configs (contains the Centinel config files)
+    -----exps (contains the experiments directories)
+
+    """
+
+    logging.info("Starting to run the experiments for each VPN")
+    logging.warn("Excluding vantage points from: %s" % (exclude_list))
+
+    # iterate over each VPN
+    vpn_dir = return_abs_path(directory, "vpns")
+    conf_dir = return_abs_path(directory, "configs")
+    auth_file = return_abs_path(".", auth_file)
+    conf_list = os.listdir(conf_dir)
+
+    if shuffle_lists:
+        shuffle(conf_list)
+
+    number = 1
+    total = len(conf_list)
+
+    for filename in conf_list:
+        logging.info("Moving onto (%d/%d) %s" % (number, total, filename))
+        print "(%d/%d) %s" % (number, total, filename)
+
+        number = number + 1
+        vpn_config = os.path.join(vpn_dir, filename)
+        centinel_config = os.path.join(conf_dir, filename)
+
+        # before starting the VPN, check if there are any experiments
+        # to run
+        config = centinel.config.Configuration()
+        config.parse_config(centinel_config)
+
+        # assuming that each VPN config file has a name like:
+        # [ip-address].ovpn, we can extract IP address from filename
+        # and use it to geolocate and fetch experiments before connecting
+        # to VPN.
+        vpn_address, extension = os.path.splitext(filename)
+        country = None
+        try:
+            meta = centinel.backend.get_meta(config.params,
+                                             vpn_address)
+            if 'country' in meta:
+                country = meta['country']
+        except Exception as exp:
+            logging.error("%s: Failed to geolocate "
+                          "%s: %s" % (filename, vpn_address, exp))
+
+        if country and exclude_list and country in exclude_list:
+            logging.info("%s: Skipping this server (%s)" % (filename, country))
+            continue
+
+        # try setting the VPN info (IP and country) to get appropriate
+        # experiemnts and input data.
+        try:
+            centinel.backend.set_vpn_info(config.params, vpn_address, country)
+        except Exception as exp:
+            logging.error("%s: Failed to set VPN info: %s" % (filename, exp))
+
+        logging.info("%s: Synchronizing." % (filename))
+        try:
+            centinel.backend.sync(config.params)
+        except Exception as exp:
+            logging.error("%s: Failed to sync: %s" % (filename, exp))
+
+        if not centinel.backend.experiments_available(config.params):
+            logging.info("%s: No experiments available." % (filename))
+            try:
+                centinel.backend.set_vpn_info(config.params, vpn_address, country)
+            except Exception as exp:
+                logging.error("Failed to set VPN info: %s" % (exp))
+            continue
+
+        logging.info("%s: Starting VPN." % (filename))
+        vpn = openvpn.OpenVPN(timeout=30, auth_file=auth_file,
+                                       config_file=vpn_config)
+        vpn.start()
+        if not vpn.started:
+            vpn.stop()
+            logging.error("%s: Failed to start VPN!" % (filename))
+            continue
+
+        logging.info("%s: Running Centinel." % (filename))
+        try:
+            client = centinel.client.Client(config.params)
+            client.setup_logging()
+            client.run()
+        except Exception as exp:
+            logging.error("%s: Error running Centinel: %s" % (filename, exp))
+
+        logging.info("%s: Stopping VPN." % (filename))
+        vpn.stop()
+
+        logging.info("%s: Synchronizing." % (filename))
+        try:
+            centinel.backend.sync(config.params)
+        except Exception as exp:
+            logging.error("%s: Failed to sync: %s" % (filename, exp))
+
+        # try setting the VPN info (IP and country) to the correct address
+        # after sync is over.
+        try:
+            centinel.backend.set_vpn_info(config.params, vpn_address, country)
+        except Exception as exp:
+            logging.error("Failed to set VPN info: %s" % (exp))
+
+def return_abs_path(directory, path):
+    """Unfortunately, Python is not smart enough to return an absolute
+    path with tilde expansion, so I writing functionality to do this
+
+    """
+    if directory is None or path is None:
+        return
+    directory = os.path.expanduser(directory)
+    return os.path.abspath(os.path.join(directory, path))
+
+def create_config_files(directory):
+
+    """For each VPN file in directory/vpns, create a new configuration
+    file and all the associated directories
+
+    Note: the expected directory structure is
+    args.directory
+    -----vpns (contains the OpenVPN config files
+    -----configs (contains the Centinel config files)
+    -----exps (contains the experiments directories)
+    -----results (contains the results)
+
+    """
+    logging.info("Starting to create config files from openvpn files")
+
+    vpn_dir = return_abs_path(directory, "vpns")
+    conf_dir = return_abs_path(directory, "configs")
+    os.mkdir(conf_dir)
+    home_dirs = return_abs_path(directory, "home")
+    os.mkdir(home_dirs)
+    for filename in os.listdir(vpn_dir):
+        configuration = centinel.config.Configuration()
+        # setup the directories
+        home_dir = os.path.join(home_dirs, filename)
+        os.mkdir(home_dir)
+        configuration.params['user']['centinel_home'] = home_dir
+        exp_dir = os.path.join(home_dir, "experiments")
+        os.mkdir(exp_dir)
+        configuration.params['dirs']['experiments_dir'] = exp_dir
+        data_dir = os.path.join(home_dir, "data")
+        os.mkdir(data_dir)
+        configuration.params['dirs']['data_dir'] = data_dir
+        res_dir = os.path.join(home_dir, "results")
+        os.mkdir(res_dir)
+        configuration.params['dirs']['results_dir'] = res_dir
+
+        log_file = os.path.join(home_dir, "centinel.log")
+        configuration.params['log']['log_file'] = log_file
+        login_file = os.path.join(home_dir, "login")
+        configuration.params['server']['login_file'] = login_file
+        configuration.params['user']['is_vpn'] = True
+
+        conf_file = os.path.join(conf_dir, filename)
+        configuration.write_out_config(conf_file)
+
+
+def run():
+    """Entry point for all uses of centinel"""
+
+    args = parse_args()
+    logging.basicConfig(filename=args.log_file,
+                        format="%(levelname)s %(asctime)s: %(message)s",
+                        level=logging.INFO)
+    if args.create_conf_dir:
+        if args.create_HMA:
+            hmaDir = return_abs_path(args.create_conf_dir, "vpns")
+            hma.create_config_files(hmaDir)
+        # create the config files for the openvpn config files
+        create_config_files(args.create_conf_dir)
+    else:
+        scan_vpns(args.directory, args.auth_file, args.exclude_list,
+                  args.shuffle_lists)
+
+if __name__ == "__main__":
+    run()
