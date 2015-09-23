c728230e3bf6769e6439736a4caf5b943fec6b07
Ashish Kumar Singh <chuck@adm-129-49-224-101.wi-fi.stonybrook.edu>
2015-09-23 19:35:49 -0400

    Replacing unittest framework with pytest framework

--- centinel/unit_test/http_unit_test.py
+++ centinel/unit_test/http_unit_test.py
@@ -1,21 +1,21 @@
-import unittest
+import pytest
 from  centinel.primitives import http
 import os
 
 
-class TestHTTPMethods(unittest.TestCase):
+class TestHTTPMethods:
 
     def test_URL_NotExist(self):
         """
         test if _get_http_request(args...) returns failure
         for an invalid url.
         """
-        file_name = "centinel/unit_test/invalid_hosts.txt"
+        file_name = "invalid_hosts.txt"
         fd = open(os.path.join(os.getcwd(), file_name), 'r')
         for line in fd:
             line = line.rstrip('\n')
             res = http._get_http_request(line)
-            self.assertIn('failure', res['response'].keys())
+            assert 'failure' in res['response'].keys()
         fd.close()
 
     def test_URL_Exist(self):
@@ -23,12 +23,12 @@ class TestHTTPMethods(unittest.TestCase):
         test if _get_http_request(args..) returns valid contents from a
         valid url.
         """
-        file_name = "centinel/unit_test/valid_hosts.txt"
+        file_name = "valid_hosts.txt"
         fd = open(os.path.join(os.getcwd(), file_name), 'r')
         for line in fd:
             line = line.rstrip('\n')
             res = http._get_http_request(line)
-            self.assertNotIn('failure', res['response'].keys())
+            assert 'failure' not in res['response'].keys()
         fd.close()
 
     def test_batch_URL(self):
@@ -36,22 +36,22 @@ class TestHTTPMethods(unittest.TestCase):
         test _get_http_request(arg...) primitive when a list of domain
         name is passed to get_requests_batch(args...).
         """
-        invalid_hosts_file_name = "centinel/unit_test/invalid_hosts.txt"
+        invalid_hosts_file_name = "invalid_hosts.txt"
         fd = open(os.path.join(os.getcwd(), invalid_hosts_file_name), 'r')
         lines = [line.rstrip('\n') for line in fd]
         results = http.get_requests_batch(lines)
         # assert failure for inValid Hosts
         for key, result in results.items():
-            self.assertIn('failure', result['response'].keys())
+            assert 'failure' in result['response'].keys()
         fd.close()
 
-        valid_hosts_file_name = "centinel/unit_test/valid_hosts.txt"
+        valid_hosts_file_name = "valid_hosts.txt"
         fd = open(os.path.join(os.getcwd(), valid_hosts_file_name), 'r')
         lines = [line.rstrip('\n') for line in fd]
         results = http.get_requests_batch(lines)
         # assert no failure for valid hosts
         for key,result in results.items():
-            self.assertNotIn('failure', result['response'].keys())
+            assert 'failure' not in result['response'].keys()
         fd.close()
 
 if __name__ == '__main__' :
