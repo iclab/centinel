94c55b7a2e4c0651676cd038203ba323df9217fd
Xinwen Wang <wxwjason@gmail.com>
2015-09-25 18:20:37 -0400

    add fixtures for reading files; write a bash script for run all tests

--- centinel/unit_test/test_traceroute.py
+++ centinel/unit_test/test_traceroute.py
@@ -1,10 +1,25 @@
 import pytest
 from centinel.primitives import traceroute
-
+import os
 
 class TestTraceRoute:
-    #1. test good case
+
+    @pytest.fixture(scope='class')
+    def invalid_domains(self):
+        cwd = os.getcwd()
+        invalid_domains = [];
+        with open(os.path.join(cwd,'centinel/data/invalid_hosts.txt')) as testfile:
+            domain = testfile.readline().rstrip('\n')
+            while domain != '':
+                invalid_domains.append(domain)
+                domain = testfile.readline().rstrip('\n')
+            testfile.close()
+        return invalid_domains
+
     def test_traceroute_valid_domain(self):
+        """
+        1. test good case
+        """
         domain = 'www.google.com'
         result = traceroute.traceroute(domain)
         #.1 test traceroute results is not none
@@ -28,8 +43,10 @@ class TestTraceRoute:
         assert result["forcefully_terminated"] is not None
         assert result["time_elapsed"] is not None
 
-    #+ test method is tcp if given method as tcp
     def test_traceroute_tcp_connection(self):
+        """
+        + test method is tcp if given method as tcp
+        """
         domain = 'www.google.com'
         result = traceroute.traceroute(domain, 'tcp')
         assert result is not None
@@ -37,8 +54,11 @@ class TestTraceRoute:
         assert 'error' not in result
 
 
-    # 2. test bad case
     def test_traceroute_invalid_domain_name(self):
+        """
+        2. test bad case
+        """
+
         #.1 given a invalid domain name
         domain = 'www.sdfadsfdasefwefewfew.fewfwefw.fwefwfsafdas.com'
         result = traceroute.traceroute(domain)
@@ -48,10 +68,13 @@ class TestTraceRoute:
         #+ test 'error' is ': name or service not known'
         assert result['error'] == ': name or service not known'
 
-    # .2 given a great number of domain names
-    @pytest.mark.skipif(True)
-    def test_traceroute_batch(self):
-        domains = []
+
+
+    def test_traceroute_batch(self, invalid_domains):
+        """
+        .2 given a great number of domain names
+        """
+        domains = [] if len(invalid_domains) is 0 else invalid_domains
         result = traceroute.traceroute_batch(domains)
         assert result is not None
         #* test 'error' is in results
