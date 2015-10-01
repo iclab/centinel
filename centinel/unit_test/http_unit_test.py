2930ee928c710cb1def05ee0b31dd9fd6ed85d7f
Ashish Kumar Singh <chuck@Ashishs-MacBook-Pro.local>
2015-10-01 08:09:25 -0400

    Adding changes as per the code review comments

--- centinel/unit_test/http_unit_test.py
+++ centinel/unit_test/http_unit_test.py
@@ -1,29 +1,32 @@
 import pytest
 from  centinel.primitives import http
 import os
-
+import sys
 
 class TestHTTPMethods:
 
-    def test_URL_NotExist(self):
+    def test_url_not_exist(capsys):
         """
         test if _get_http_request(args...) returns failure
         for an invalid url.
         """
-        file_name = "invalid_hosts.txt"
+        file_name = "../data/invalid_hosts.txt"
         fd = open(os.path.join(os.getcwd(), file_name), 'r')
+        #sys.stdout.write("Hello World!\n")
         for line in fd:
             line = line.rstrip('\n')
             res = http._get_http_request(line)
+            print res;
             assert 'failure' in res['response'].keys()
+
         fd.close()
 
-    def test_URL_Exist(self):
+    def test_url_exist(self):
         """
         test if _get_http_request(args..) returns valid contents from a
         valid url.
         """
-        file_name = "valid_hosts.txt"
+        file_name = "../data/valid_hosts.txt"
         fd = open(os.path.join(os.getcwd(), file_name), 'r')
         for line in fd:
             line = line.rstrip('\n')
@@ -31,12 +34,12 @@ class TestHTTPMethods:
             assert 'failure' not in res['response'].keys()
         fd.close()
 
-    def test_batch_URL(self):
+    def test_batch_url(self):
         """
         test _get_http_request(arg...) primitive when a list of domain
         name is passed to get_requests_batch(args...).
         """
-        invalid_hosts_file_name = "invalid_hosts.txt"
+        invalid_hosts_file_name = "../data/invalid_hosts.txt"
         fd = open(os.path.join(os.getcwd(), invalid_hosts_file_name), 'r')
         lines = [line.rstrip('\n') for line in fd]
         results = http.get_requests_batch(lines)
@@ -45,7 +48,7 @@ class TestHTTPMethods:
             assert 'failure' in result['response'].keys()
         fd.close()
 
-        valid_hosts_file_name = "valid_hosts.txt"
+        valid_hosts_file_name = "../data/valid_hosts.txt"
         fd = open(os.path.join(os.getcwd(), valid_hosts_file_name), 'r')
         lines = [line.rstrip('\n') for line in fd]
         results = http.get_requests_batch(lines)
@@ -53,3 +56,4 @@ class TestHTTPMethods:
         for key,result in results.items():
             assert 'failure' not in result['response'].keys()
         fd.close()
+
