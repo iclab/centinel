234664129158ea678bf028c393257bbedb0183fc
Adrian Li <lianke123321@gmail.com>
2015-11-10 22:43:15 -0500

    Added random User-Agent function. Moved this to the beginning of get_request, and changed list to dict when creating headers variable

--- centinel/utils.py
+++ centinel/utils.py
@@ -3,6 +3,26 @@ import glob
 import hashlib
 import os.path
 
+# Use this list to randomly choose a User-Agent string
+user_agent_pool = [
+    # Chrome
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36",
+    # Firefox
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+    # Internet Explorer
+    "Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko",
+    # Maxthon
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/533.1 "
+    "(KHTML, like Gecko) Maxthon/3.0.8.2 Safari/533.1",
+    # Opera
+    "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
+    # Safari
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 "
+    "(KHTML, like Gecko) Version/7.0.3 Safari/7046A194A",
+    # MyIE2
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; MyIE2; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0)"
+]
+
 
 def hash_folder(folder, regex='[!_]*'):
     """
@@ -49,7 +69,7 @@ def compute_files_to_download(client_hashes, server_hashes):
 
     Note: we will mark a file for deletion if it is not available on
     the server
-    
+
     """
     to_dload, to_delete = [], []
     for filename in server_hashes:
