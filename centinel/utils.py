451b2455589eef17d715ee480248df2d68137b08
Adrian Li <lianke123321@gmail.com>
2015-11-10 22:19:20 -0500

    refactor centinel/utils.py

--- centinel/utils.py
+++ centinel/utils.py
@@ -5,11 +5,12 @@ import os.path
 
 
 def hash_folder(folder, regex='[!_]*'):
-    """Get the md5 sum of each file in the folder and return to the user
+    """
+    Get the md5 sum of each file in the folder and return to the user
 
-    Params:
-    folder- the folder to compute the sums over
-    regex- an expression to limit the files we match
+    :param folder: the folder to compute the sums over
+    :param regex: an expression to limit the files we match
+    :return:
 
     Note: by default we will hash every file in the folder
 
@@ -31,25 +32,24 @@ def hash_folder(folder, regex='[!_]*'):
 
 
 def compute_files_to_download(client_hashes, server_hashes):
-    """Given a dictionary of file hashes from the client and the
+    """
+    Given a dictionary of file hashes from the client and the
     server, specify which files should be downloaded from the server
 
-    Params:
-    client_hashes- a dictionary where the filenames are keys and the
-         values are md5 hashes as strings
-    server_hashes- a dictionary where the filenames are keys and the
-         values are md5 hashes as strings
-
-    Return values: a list of 2 lists -> [to_dload, to_delete]
-    to_dload- a list of filenames to get from the server
-    to_delete- a list of filenames to delete from the folder
+    :param client_hashes: a dictionary where the filenames are keys and the
+                          values are md5 hashes as strings
+    :param server_hashes: a dictionary where the filenames are keys and the
+                          values are md5 hashes as strings
+    :return: a list of 2 lists -> [to_dload, to_delete]
+             to_dload- a list of filenames to get from the server
+             to_delete- a list of filenames to delete from the folder
 
     Note: we will get a file from the server if a) it is not on the
     client or b) the md5 differs between the client and server
 
     Note: we will mark a file for deletion if it is not available on
     the server
-
+    
     """
     to_dload, to_delete = [], []
     for filename in server_hashes:
