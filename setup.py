20186452c6858ec982afae8c9541ca425b3a634a
Abbas Razaghpanah <arazaghpanah@cs.stonybrook.edu>
2016-02-12 11:56:51 -0500

    bumping version to 1.5.4.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ online information controls, and Internet censorship."""
 
 setup(
     name="centinel",
-    version="0.1.5.4.1",
+    version="0.1.5.4.2",
     author="ICLab Developers",
     author_email="info@iclab.org",
     description=DESCRIPTION,
