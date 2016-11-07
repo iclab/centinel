f774a713f5ee10e6706a945174ef39b66b0ca1cd
Abbas Razaghpanah <arazaghpanah@cs.stonybrook.edu>
2016-11-07 18:37:19 -0500

    added parallel TCP conncet

--- centinel/primitives/tcp_connect.py
+++ centinel/primitives/tcp_connect.py
@@ -7,12 +7,16 @@
 from datetime import datetime
 import logging
 import socket
+import time
+import threading
 
-def tcp_connect(host, port):
+def tcp_connect(host, port, external=None, log_prefix=''):
     result = {
         "host" : host,
         "port" : port
     }
+
+    logging.debug(log_prefix + "Testing TCP connect to %s:%s..." % (host, port))
 
     try:
         ip = socket.gethostbyname(host)
@@ -36,7 +40,57 @@ def tcp_connect(host, port):
         elapsed = int((end_time - start_time).total_seconds() * 1000)
         result["failure"] = str(err)
 
-
-    logging.debug("time: " + str(elapsed) + "ms")
     result["time"] = str(elapsed)
+
+    # the external result is used when threading to store
+    # the results in the list container provided.
+    if external is not None and type(external) is dict:
+        external[host + ":" + str(port)] = result
+
     return result
+
+def tcp_connect_batch(input_list, delay_time=0.1, max_threads=100):
+    """
+    This is a parallel version of the TCP connect primitive.
+
+    :param input_list: the input is a list of host/port pairs
+    :param delay_time: delay before starting each thread
+    :param max_threads: maximum number of concurrent threads
+    :return:
+    """
+    results = {}
+    threads = []
+    thread_error = False
+    thread_wait_timeout = 200
+    ind = 1
+    total_item_count = len(input_list)
+    for host,port in input_list:
+        wait_time = 0
+        while threading.active_count() > max_threads:
+            time.sleep(1)
+            wait_time += 1
+            if wait_time > thread_wait_timeout:
+                thread_error = True
+                break
+
+        if thread_error:
+            results["error"] = "Threads took too long to finish."
+            break
+
+        # add just a little bit of delay before starting the thread
+        # to avoid overwhelming the connection.
+        time.sleep(delay_time)
+
+        log_prefix = "%d/%d: " % (ind, total_item_count)
+        thread = threading.Thread(target=tcp_connect,
+                                  args=(host, port,
+                                        results, log_prefix))
+        ind += 1
+        thread.setDaemon(1)
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join(thread_wait_timeout)
+
+    return results
