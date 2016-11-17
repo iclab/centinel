05bb91ab1d6aa4d248978c140fb35201cd949c00
Abbas Razaghpanah <arazaghpanah@cs.stonybrook.edu>
2016-11-17 15:21:04 -0500

    added guard block to VPN

--- centinel/vpn/cli.py
+++ centinel/vpn/cli.py
@@ -22,6 +22,8 @@ import centinel.vpn.ipvanish as ipvanish
 import centinel.vpn.purevpn as purevpn
 import centinel.vpn.vpngate as vpngate
 
+PID_FILE = "/tmp/centinel.lock"
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -440,6 +442,38 @@ def experiments_available(config):
 
 
 def run():
+    if os.path.isfile(PID_FILE):
+        with open(PID_FILE) as f:
+            pid = f.read()
+            print "Centinel already running (PID = %s)" % pid
+            print "Lock file address: %s" % PID_FILE
+        sys.exit(1)
+
+    try:
+        f = open(PID_FILE, "w")
+        f.write("%d" % os.getpid())
+        f.close()
+    except Exception as exp:
+        sys.stderr.write('Error to writing the'
+                         ' lock file: %s\n' % exp)
+        sys.exit(1)
+
+    try:
+        _run()
+    except SystemExit:
+        pass
+    except KeyboardInterrupt:
+        print "Keyboard interrupt received, exiting..."
+    except Exception as exp:
+        sys.stderr.write("%s" % exp)
+
+    try:
+        os.remove(PID_FILE)
+    except Exception as exp:
+        sys.stderr.write("Failed to remove lock file %s: %s" % (PID_FILE, exp))
+
+
+def _run():
     """Entry point for all uses of centinel"""
     args = parse_args()
 
