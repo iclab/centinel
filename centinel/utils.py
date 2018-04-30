d08ea3187dfb2a3945ebe41d3b111dc4820451f6
Siva Prasad <MagnumOpus21@users.noreply.github.com>
2018-04-30 11:17:16 -0400

    Replaced HTTP User-Agents
    Uses new User agents for the tests.

--- centinel/utils.py
+++ centinel/utils.py
@@ -12,15 +12,10 @@ user_agent_pool = [
     # Internet Explorer
     "Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko",
     # Maxthon
-    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/533.1 "
-    "(KHTML, like Gecko) Maxthon/3.0.8.2 Safari/533.1",
     # Opera
     "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
     # Safari
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 "
-    "(KHTML, like Gecko) Version/7.0.3 Safari/7046A194A",
-    # MyIE2
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; MyIE2; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0)"
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"
 ]
 
 
