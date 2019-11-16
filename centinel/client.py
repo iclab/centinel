f78eea0a6410daa237fa2f86f3b05aa8a8600e5f
shinyoungcho <shicho@cs.stonybrook.edu>
2019-11-15 21:20:31 -0500

    keep vpn info in meta

--- centinel/client.py
+++ centinel/client.py
@@ -239,6 +239,13 @@ class Client:
                 logging.exception("Error fetching metadata for "
                                   "%s: %s" % (name, exception))
                 results["meta_exception"] = str(exception)
+		
+	    if 'hostname' in self.configs['user']:
+		results['meta]['vpn_name'] = self.configs['user']['hostname']
+	    if 'connected_ip' in self.configs['user']:
+                results['meta]['vpn_ip'] = self.configs['user']['connected_ip']
+	    if 'claimed_country' in self.configs['user']:
+		results['meta]['vpn_claimed_country'] = self.configs['user']['claimed_country']
 
             if schedule_name is not None:
                 results["meta"]["schedule_name"] = schedule_name
